e31599a3f9f94140b40def1775458245ab2d9e2e
leggod1004 <leg429@naver.com>
2018-11-02 16:38:47 +0900

    Add models.js

---
+++ models.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    'node_api_codelab',
+    'root',
+    '1234',
+    {
+        'dialect': 'mysql'
+    }
+);
+
+const User = sequelize.define('user', {
+    name: Sequelize.STRING
+});
+
+module.exports = {
+    sequelize: sequelize,
+    User: User
+}
